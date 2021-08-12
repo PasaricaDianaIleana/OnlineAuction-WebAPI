4458beb55dcf5ca4deb80f939be30d3b831546b1
Pasarica Diana <pasaricadiana@yahoo.com>
2021-08-12 19:29:51 +0300

    Login method

--- AuctionWebApi/Controllers/UserController.cs
+++ AuctionWebApi/Controllers/UserController.cs
@@ -2,7 +2,11 @@
 using DataLibrary.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AuctionWebApi.Controllers
@@ -48,6 +52,37 @@ namespace AuctionWebApi.Controllers
           
             }
               return BadRequest();
+        }
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult> Login([FromBody] UserLoginDTO userLogin)
+        {
+            var user = await _userManager.FindByNameAsync(userLogin.UserName);
+            var password = await _userManager.CheckPasswordAsync(user, userLogin.Password);
+              if(user!=null && password)
+            {
+                
+                var token = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                        new Claim("UserId", user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials=new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456")),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(token);
+                var writeToken = tokenHandler.WriteToken(securityToken);
+                return Ok(new { writeToken, user.Id });
+            }
+            else
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+        
+        
         }
     }
 }
